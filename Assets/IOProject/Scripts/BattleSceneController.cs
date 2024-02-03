ac54f0d7c1d4bebe055e0af625f89565d2059b2e
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-03 23:12:13 +0900

    Add StrixSceneInfo GameObject and GameCameraController script

--- Assets/IOProject/Scripts/BattleSceneController.cs
+++ Assets/IOProject/Scripts/BattleSceneController.cs
@@ -1,6 +1,13 @@
+using HK.Framework.BootSystems;
+using UnityEngine;
+
 namespace IOProject
 {
     public class BattleSceneController : SceneController
     {
+        async void Start()
+        {
+            await BootSystem.IsReady;
+        }
     }
 }
---
+++ Assets/IOProject/Scripts/GameCameraController.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace IOProject
+{
+    public class GameCameraController : MonoBehaviour
+    {
+    }
+}
