7acbeb143771f9fcda07a78c2b35f9ca6842d2b0
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-03 23:14:15 +0900

    Add player actor and game camera controller instantiation in BattleSceneController

--- Assets/IOProject/Scripts/BattleSceneController.cs
+++ Assets/IOProject/Scripts/BattleSceneController.cs
@@ -1,13 +1,23 @@
 using HK.Framework.BootSystems;
+using IOProject.ActorControllers;
 using UnityEngine;
 
 namespace IOProject
 {
     public class BattleSceneController : SceneController
     {
+        [SerializeField]
+        private Actor playerActorPrefab;
+
+        [SerializeField]
+        private GameCameraController gameCameraControllerPrefab;
+
         async void Start()
         {
             await BootSystem.IsReady;
+
+            var playerActor = Instantiate(this.playerActorPrefab);
+            var gameCameraController = Instantiate(this.gameCameraControllerPrefab);
         }
     }
 }
