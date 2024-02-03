57b651fb3831d34593cfc99da5f679f0204af409
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-04 01:50:15 +0900

    Add player actor controller to BattleSceneController

--- Assets/IOProject/Scripts/BattleSceneController.cs
+++ Assets/IOProject/Scripts/BattleSceneController.cs
@@ -19,6 +19,8 @@ namespace IOProject
             var playerActor = Instantiate(this.playerActorPrefab);
             var gameCameraController = Instantiate(this.gameCameraControllerPrefab);
             gameCameraController.SetTarget(playerActor.transform);
+            var playerActorController = new PlayerActorController();
+            playerActorController.Attach(playerActor);
         }
     }
 }
