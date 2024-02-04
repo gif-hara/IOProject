0f72626201c25eb668bbfa3fa02236e9497a01d5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-04 15:58:10 +0900

    Add new component and network functionality

--- Assets/IOProject/Scripts/GameSceneController.cs
+++ Assets/IOProject/Scripts/GameSceneController.cs
@@ -1,6 +1,8 @@
 using Cysharp.Threading.Tasks;
 using HK.Framework.BootSystems;
 using IOProject.ActorControllers;
+using SoftGear.Strix.Net.Logging;
+using SoftGear.Strix.Unity.Runtime;
 using UnityEngine;
 
 namespace IOProject
@@ -19,6 +21,7 @@ namespace IOProject
         async void Start()
         {
             await HK.Framework.BootSystems.BootSystem.IsReady;
+            ConnectMasterServer();
             TinyServiceLocator.RegisterAsync<IInputController>(new InputController(), this.destroyCancellationToken).Forget();
             var playerActor = playerActorPrefab.Spawn(new ActorModel());
             var gameCameraController = Instantiate(this.gameCameraControllerPrefab);
@@ -27,6 +30,61 @@ namespace IOProject
             var playerActorController = new PlayerActorController();
             playerActorController.Attach(playerActor);
             Instantiate(this.stageChunkPrefab);
+        }
+
+        private void ConnectMasterServer()
+        {
+            LogManager.Instance.Filter = Level.INFO;
+            StrixNetwork.instance.applicationId = "686bc912-86ae-4134-a39c-cb4884d95eff";
+            StrixNetwork.instance.ConnectMasterServer("63da7b783afd86c8787cffbf.game.strixcloud.net", 9122,
+                args =>
+                {
+                    Debug.Log($"connectEventHandler: {args}");
+                    JoinRandomRoom();
+                },
+                args =>
+                {
+                    Debug.Log($"errorEventHandler: {args}");
+                }
+            );
+        }
+
+        private void JoinRandomRoom()
+        {
+            StrixNetwork.instance.JoinRandomRoom("Test",
+                args =>
+                {
+                    Debug.Log($"joinRoomEventHandler: {args}");
+                },
+                args =>
+                {
+                    Debug.Log($"failreRoomEventHandler: {args}");
+                    CreateRoom();
+                }
+            );
+        }
+
+        private void CreateRoom()
+        {
+            var roomProperties = new RoomProperties
+            {
+                capacity = 100,
+                name = "TestRoom",
+            };
+            var roomMemberProperties = new RoomMemberProperties
+            {
+                name = "Test",
+            };
+            StrixNetwork.instance.CreateRoom(roomProperties, roomMemberProperties,
+                args =>
+                {
+                    Debug.Log($"createRoomEventHandler: {args}");
+                },
+                args =>
+                {
+                    Debug.Log($"failureCreateRoomEventHandler: {args}");
+                }
+            );
         }
     }
 }
