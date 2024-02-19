78df9722a680eac1a775aadc55c2334e21d884a5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-19 23:07:26 +0900

    Projectile.csの変更：OnTriggerEnterメソッドを追加

--- Assets/IOProject/Scripts/Projectile.cs
+++ Assets/IOProject/Scripts/Projectile.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using IOProject.ActorControllers;
 using UnityEngine;
 using UnitySequencerSystem;
 
@@ -13,10 +14,21 @@ namespace IOProject
         [SerializeReference, SubclassSelector]
         private List<ISequence> onEnableSequence;
 
+        [SerializeReference, SubclassSelector]
+        private List<ISequence> onTriggerEnterSequence;
+
         void OnEnable()
         {
             var container = new Container();
             var sequencer = new Sequencer(container, onEnableSequence);
+            sequencer.PlayAsync(destroyCancellationToken).Forget();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            var container = new Container();
+            container.Register("TargetActor", other.GetComponentInParent<Actor>());
+            var sequencer = new Sequencer(container, onTriggerEnterSequence);
             sequencer.PlayAsync(destroyCancellationToken).Forget();
         }
     }
