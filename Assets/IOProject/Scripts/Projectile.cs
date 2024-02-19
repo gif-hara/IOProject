35e7ab104fb5d92dd9bfe229ba0c420eb0589f9b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-19 22:42:26 +0900

    Projectile.csに非同期再生の機能を追加

--- Assets/IOProject/Scripts/Projectile.cs
+++ Assets/IOProject/Scripts/Projectile.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnitySequencerSystem;
 
@@ -11,5 +12,12 @@ namespace IOProject
     {
         [SerializeReference, SubclassSelector]
         private List<ISequence> sequences;
+
+        void OnEnable()
+        {
+            var container = new Container();
+            var sequencer = new Sequencer(container, this.sequences);
+            sequencer.PlayAsync(destroyCancellationToken).Forget();
+        }
     }
 }
