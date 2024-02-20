ed4fb5dd1df7a4918f87ec6888a5ccd772ee4652
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-20 23:48:45 +0900

    ゲームデザインデータの追加と更新

---
+++ Assets/IOProject/Scripts/GameDesignData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace IOProject
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [CreateAssetMenu(fileName = "GameDesignData", menuName = "IOProject/GameDesignData")]
+    public sealed class GameDesignData : ScriptableObject
+    {
+        [SerializeField]
+        private int stageChunkSize;
+
+        public int StageChunkSize => stageChunkSize;
+
+        [SerializeField]
+        private int stageViewRange;
+
+        public int StageViewRange => stageViewRange;
+    }
+}
