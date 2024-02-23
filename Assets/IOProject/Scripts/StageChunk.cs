935ec8952fbdee857b36385e45300ad652fdf728
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-23 22:09:45 +0900

    StageChunk.csの変更: usingステートメントの追加、Setupメソッドの修正

--- Assets/IOProject/Scripts/StageChunk.cs
+++ Assets/IOProject/Scripts/StageChunk.cs
@@ -1,4 +1,6 @@
-using IOProject.ActorControllers;
+using System;
+using System.Threading;
+using R3;
 using UnityEngine;
 
 namespace IOProject
@@ -10,9 +12,31 @@ namespace IOProject
     {
         public StageChunkModel Model { get; private set; }
 
+        private CancellationTokenSource scope;
+
         public void Setup(StageChunkModel model)
         {
+            scope?.Cancel();
+            scope?.Dispose();
+            scope = new CancellationTokenSource();
             this.Model = model;
+            this.Model.OnAddDamageMap
+                .Subscribe(networkInstanceId =>
+                {
+                    this.Model.DamageMap[networkInstanceId]
+                        .Subscribe(damage =>
+                        {
+                            Debug.Log($"StageChunkModel.OnAddDamageMap: PositionId = {Model.PositionId}, networkInstanceId = {networkInstanceId}, damage = {damage}", this);
+                        })
+                        .RegisterTo(scope.Token);
+                })
+                .RegisterTo(scope.Token);
+            this.Model.OnOccupied
+                .Subscribe(networkInstanceId =>
+                {
+                    Debug.Log($"StageChunkModel.OnOccupied: PositionId = {Model.PositionId}, networkInstanceId = {networkInstanceId}", this);
+                })
+                .RegisterTo(scope.Token);
         }
     }
 }
