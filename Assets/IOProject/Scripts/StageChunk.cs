7eaeb4b769abe23b0cd4e8f81b87d73b98a9d97b
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-23 21:55:36 +0900

    StageChunkクラスの修正: Modelプロパティを公開

--- Assets/IOProject/Scripts/StageChunk.cs
+++ Assets/IOProject/Scripts/StageChunk.cs
@@ -8,22 +8,11 @@ namespace IOProject
     /// </summary>
     public sealed class StageChunk : MonoBehaviour
     {
-        private StageChunkModel model;
+        public StageChunkModel Model { get; private set; }
 
         public void Setup(StageChunkModel model)
         {
-            this.model = model;
-        }
-
-        public void TakeDamage(Actor actor, int damage)
-        {
-            if (!actor.isLocal)
-            {
-                return;
-            }
-
-            this.model.AddDamageMap(actor.strixReplicator.networkInstanceId, damage);
-            Debug.Log($"TakeDamage: actor = {actor}, damage = {damage}", this);
+            this.Model = model;
         }
     }
 }
