858ef0389c0627cd3be5ea009465f1c7583d639f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-28 21:53:30 +0900

    ActorクラスとActorNetworkControllerクラスを変更しました

--- Assets/IOProject/Scripts/ActorControllers/Actor.cs
+++ Assets/IOProject/Scripts/ActorControllers/Actor.cs
@@ -1,50 +1,27 @@
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
-using R3;
-using R3.Triggers;
-using SoftGear.Strix.Unity.Runtime;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace IOProject.ActorControllers
 {
-    public class Actor : StrixBehaviour
+    public class Actor : MonoBehaviour
     {
         [SerializeField]
         private List<ActorLocatorController.LocatorData> locators;
 
+        [SerializeField]
+        private ActorNetworkController networkController;
+
         public ActorLocatorController LocatorController { get; private set; }
 
         public IActorPostureController PostureController { get; private set; }
+
+        public ActorNetworkController NetworkController => this.networkController;
 
         void Awake()
         {
             this.LocatorController = new ActorLocatorController(locators);
             this.PostureController = GetComponent<IActorPostureController>();
             this.PostureController.Setup(this);
-            var gameNetworkController = TinyServiceLocator.Resolve<GameNetworkController>();
-            this.UpdateAsObservable()
-                .Subscribe(_ =>
-                {
-                    if (!isLocal)
-                    {
-                        return;
-                    }
-                    if (Keyboard.current.qKey.wasPressedThisFrame)
-                    {
-                        Debug.Log("qKey.wasPressedThisFrame", this);
-                        gameNetworkController.SendRoomRelayAsync(new NetworkMessage.Helloworld { message = $"Hello, world! {this.strixReplicator.networkInstanceId}" }).Forget();
-                    }
-                })
-                .RegisterTo(this.destroyCancellationToken);
-            gameNetworkController.RoomRelayAsObservable()
-                .WhereOwner(this)
-                .MatchMessage<NetworkMessage.Helloworld>()
-                .Subscribe(x =>
-                {
-                    Debug.Log($"RoomRelayAsObservable: {x.message}", this);
-                })
-                .RegisterTo(this.destroyCancellationToken);
         }
 
         public Actor Spawn()
@@ -52,77 +29,6 @@ namespace IOProject.ActorControllers
             var actor = Instantiate(this);
             ActorEvents.OnSpawned.Publish(actor);
             return actor;
-        }
-
-        public void GiveDamage(Actor targetActor, int damage)
-        {
-            if (!isLocal)
-            {
-                return;
-            }
-            targetActor.RpcToAll(nameof(TakeDamage), this.strixReplicator.networkInstanceId, damage);
-        }
-
-        public void GiveDamage(StageChunk stageChunk, int damage)
-        {
-            if (!isLocal)
-            {
-                return;
-            }
-            var positionId = stageChunk.Model.PositionId;
-            RpcToRoomOwner(nameof(RpcGiveDamageStageChunk), positionId.x, positionId.y, damage);
-        }
-
-        public void SendPosition()
-        {
-            if (!isLocal)
-            {
-                return;
-            }
-            RpcToAll(
-                nameof(SyncPosition),
-                transform.position.x,
-                transform.position.y,
-                transform.position.z,
-                PostureController.PositionIdReactiveProperty.CurrentValue.x,
-                PostureController.PositionIdReactiveProperty.CurrentValue.y
-            );
-        }
-
-        [StrixRpc]
-        public void SyncPosition(float x, float y, float z, int positionIdX, int positionIdY)
-        {
-            if (isLocal)
-            {
-                return;
-            }
-            if (!TinyServiceLocator.Contains<Actor>("LocalActor"))
-            {
-                return;
-            }
-            var positionId = new Vector2Int(positionIdX, positionIdY);
-            PostureController.SyncPositionId(positionId);
-            var localPositionId = TinyServiceLocator.Resolve<Actor>("LocalActor").PostureController.PositionIdReactiveProperty.CurrentValue;
-            var gameDesignData = TinyServiceLocator.Resolve<GameDesignData>();
-            var diff = PostureController.PositionIdReactiveProperty.CurrentValue - localPositionId;
-            transform.position = new Vector3(
-                x + diff.x * gameDesignData.StageChunkSize,
-                y,
-                z + diff.y * gameDesignData.StageChunkSize
-            );
-            Debug.Log($"SyncPosition: positionId = {positionId}, localPositionId = {localPositionId}, diff = {diff}, transform.position = {transform.position}", this);
-        }
-
-        [StrixRpc]
-        public void TakeDamage(long attackerNetworkInstanceId, int damage)
-        {
-            Debug.Log($"TakeDamage: attackerNetworkInstanceId = {attackerNetworkInstanceId}, myNetworkInstanceId = {this.strixReplicator.networkInstanceId}, damage = {damage}", this);
-        }
-
-        [StrixRpc]
-        public void RpcGiveDamageStageChunk(int positionX, int positionY, int damage)
-        {
-            TinyServiceLocator.Resolve<Stage>().TakeDamageStageChunk(this.strixReplicator.networkInstanceId, new Vector2Int(positionX, positionY), damage);
         }
     }
 }
---
+++ Assets/IOProject/Scripts/ActorControllers/ActorNetworkController.cs
@@ -0,0 +1,53 @@
+using Cysharp.Threading.Tasks;
+using R3;
+using R3.Triggers;
+using SoftGear.Strix.Unity.Runtime;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+
+namespace IOProject.ActorControllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class ActorNetworkController : StrixBehaviour
+    {
+        void Awake()
+        {
+            var gameNetworkController = TinyServiceLocator.Resolve<GameNetworkController>();
+            this.UpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    if (!isLocal)
+                    {
+                        return;
+                    }
+                    if (Keyboard.current.qKey.wasPressedThisFrame)
+                    {
+                        Debug.Log("qKey.wasPressedThisFrame", this);
+                        gameNetworkController.SendRoomRelayAsync(new NetworkMessage.Helloworld { message = $"Hello, world! {this.strixReplicator.networkInstanceId}" }).Forget();
+                    }
+                })
+                .RegisterTo(this.destroyCancellationToken);
+            gameNetworkController.RoomRelayAsObservable()
+                .WhereOwner(this)
+                .MatchMessage<NetworkMessage.Helloworld>()
+                .Subscribe(x =>
+                {
+                    Debug.Log($"RoomRelayAsObservable: {x.message}", this);
+                })
+                .RegisterTo(this.destroyCancellationToken);
+        }
+
+        public UniTask SendRoomRelayAsync<T>(T message)
+        {
+            if (!isLocal)
+            {
+                Debug.LogError("SendRoomRelayAsync: !isLocal", this);
+                return UniTask.CompletedTask;
+            }
+            return TinyServiceLocator.Resolve<GameNetworkController>().SendRoomRelayAsync(message);
+        }
+    }
+}
