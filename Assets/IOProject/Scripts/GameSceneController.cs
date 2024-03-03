64aa96abc5ca71ddb06b6bc05d0f67cf900f9494
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-03-03 11:23:11 +0900

    GameSceneController.csの変更: インスタンス化の修正

--- Assets/IOProject/Scripts/GameSceneController.cs
+++ Assets/IOProject/Scripts/GameSceneController.cs
@@ -43,7 +43,7 @@ namespace IOProject
 
             if (StrixNetwork.instance.isRoomOwner)
             {
-                var _ = Instantiate(this.stagePrefab);
+                _ = Instantiate(this.stagePrefab);
             }
 
             await UniTask.WaitUntil(() => TinyServiceLocator.Contains<Stage>());
